0c43742c40b8610bae5e98494c89086471d28a86
Drew Tadlock <tadlockd7379@student.faytechcc.edu>
2023-01-09 17:02:53 -0500

    Census Assignment

---
+++ Census/Census_TadlockDrew.py
@@ -0,0 +1,3 @@
+print('My name is Drew Tadlock.')
+print('My major is Information Technology/Computer Programming & Development.')
+print('I\'m taking this class because I need the credit and I want to learn more python.')
